a46bacef8fc20581c6a35dd3a104c4347435c4b1
aquilonDev1 <aquilondev.proton.me>
2024-08-19 17:28:02 +0200

    add all inventories

--- main.lua
+++ main.lua
@@ -1,2 +1,10 @@
----test
-print("test")
+-- get all invetory peripheral
+local inventories = {}
+for key, name in pairs(peripheral.getNames()) do
+  local p = peripheral.wrap(name)
+  if p.size then
+    inventories[#inventories + 1] = p
+  end
+end
+
+print(textutils.serialiseJSON(inventories))
