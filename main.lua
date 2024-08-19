e0cbc0b5eebe4b266b9f5b535d6cad620184b79c
aquilonDev1 <aquilondev.proton.me>
2024-08-19 17:40:01 +0200

    c

--- main.lua
+++ main.lua
@@ -10,7 +10,7 @@ end
 local handle = fs.open("./items.json", "w")
 for key, inv in pairs(inventories) do
   for slot, item in pairs(inv.list()) do
-    print(item)
+    print(textutils.serialiseJSON(item))
     handle.write(textutils.serialiseJSON({ count = item.count, displayName = item.displayName, slot = slot }))
   end
 end
